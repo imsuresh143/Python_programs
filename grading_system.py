ccda5b00a40528abf3cbcc6593fa36f9e83d1a46
Suresh Rajpoot <itssr65@gmail.com>
2025-04-07 17:51:52 +0530

    Create grading_system.py

---
+++ grading_system.py
@@ -0,0 +1,12 @@
+name=input("enter the subject")
+mark=int(input("Enter the mark"))
+if mark>=85:
+    print("grade A")
+elif mark>=75:
+    print("grade B")
+elif mark>=60:
+    print("grade C")
+elif mark>=45:
+    print("grade D") 
+else:
+    print("fail")     
